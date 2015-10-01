8a7116d5c738ebfc606a151c111e3a922dfabad0
Youdong Zhang <youdongz@berkeley.edu>
2015-09-30 19:25:41 -0700

    push

--- src/translate_utils.c
+++ src/translate_utils.c
@@ -58,6 +58,15 @@ int translate_num(long int* output, const char* str, long int lower_bound,
     if (!str || !output) {
         return -1;
     }
+    
+    long int* temp;
+    temp = strtol(str, NULL, 0);
+
+    if (*temp != 0L && *temp >= lower_bound && temp <= upper_bound) {
+      *output = *temp;
+      return 0; 
+    }
+
     /* YOUR CODE HERE */
     return -1;
 }
@@ -72,7 +81,39 @@ int translate_reg(const char* str) {
     else if (strcmp(str, "$0") == 0)    return 0;
     else if (strcmp(str, "$at") == 0)   return 1;
     else if (strcmp(str, "$v0") == 0)   return 2;
+    else if (strcmp(str, "$v1") == 0)   return 3;
     else if (strcmp(str, "$a0") == 0)   return 4;
-    /* YOUR CODE HERE */
+    else if (strcmp(str, "$a1") == 0)   return 5;
+    else if (strcmp(str, "$a2") == 0)   return 6;
+    else if (strcmp(str, "$a3") == 0)   return 7;
+
+    else if (strcmp(str, "$t0") == 0)   return 8;
+    else if (strcmp(str, "$t1") == 0)   return 9;
+    else if (strcmp(str, "$t2") == 0)   return 10;
+    else if (strcmp(str, "$t3") == 0)   return 11;
+    else if (strcmp(str, "$t4") == 0)   return 12;
+    else if (strcmp(str, "$t5") == 0)   return 13;
+    else if (strcmp(str, "$t6") == 0)   return 14;
+    else if (strcmp(str, "$t7") == 0)   return 15;
+
+    else if (strcmp(str, "$s0") == 0)   return 16;
+    else if (strcmp(str, "$s1") == 0)   return 17;
+    else if (strcmp(str, "$s2") == 0)   return 18;
+    else if (strcmp(str, "$s3") == 0)   return 19;
+    else if (strcmp(str, "$s4") == 0)   return 20;
+    else if (strcmp(str, "$s5") == 0)   return 21;
+    else if (strcmp(str, "$s6") == 0)   return 22;
+    else if (strcmp(str, "$s7") == 0)   return 23;
+
+    else if (strcmp(str, "$t8") == 0)   return 24;
+    else if (strcmp(str, "$t9") == 0)   return 25;
+
+    else if (strcmp(str, "$k0") == 0)   return 26;
+    else if (strcmp(str, "$k1") == 0)   return 27;
+
+    else if (strcmp(str, "$gp") == 0)   return 28;
+    else if (strcmp(str, "$sp") == 0)   return 29;
+    else if (strcmp(str, "$fp") == 0)   return 30;
+    else if (strcmp(str, "$ra") == 0)   return 31;    
     else                                return -1;
 }
